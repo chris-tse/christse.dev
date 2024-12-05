0b61b36e90d3c21aa2ac6271d5487b3c7cb01654
Chris Tse <chris@chris-tse.com>
2024-12-05 16:43:49 -0600

    add react perf talk

---
+++ .prettierrc.mjs
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Config} */
+export default {
+	plugins: ['prettier-plugin-astro'],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+}
