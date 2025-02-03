c7096aca44904dc216b9fa63f518f54b2682fbe6
Chris Tse <chris@chris-tse.com>
2025-02-02 21:03:48 -0600

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	trailingSlash: 'never',
+    site: 'https://christse.dev',
+    integrations: [tailwind(), sitemap()],
+    trailingSlash: 'never',
 })
